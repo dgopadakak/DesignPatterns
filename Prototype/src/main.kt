e542707e5cbd854dc415d16ca751939c15e5d8a0
dgopadakak <dgopadakakv3@gmail.com>
2024-07-05 12:06:05 +0300

    Паттерн прототип добавлен

--- Prototype/src/main.kt
+++ Prototype/src/main.kt
@@ -1,3 +1,33 @@
-fun main() {
+internal fun main() {
+    val master = Project(1, "Super Project", "<html><body>Hello World</body></html>")
+    println(master)
 
+    val masterClone1 = master.myCopyImpl()
+    val masterClone2 = ProjectFactory(master).cloneProject()
+    println(masterClone1)
+    println(masterClone2)
+}
+
+internal class ProjectFactory(
+    private val project: Project
+) {
+    fun cloneProject(): Project {
+        return project.myCopyImpl() as Project
+    }
+}
+
+internal data class Project(
+    val id: Int,
+    val name: String,
+    val code: String
+) : MyCopyable {
+
+    override fun myCopyImpl(): Any {
+        return Project(id, name, code)
+    }
+}
+
+
+internal interface MyCopyable {
+    fun myCopyImpl(): Any
 }
